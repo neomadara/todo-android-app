ec8682587d82109b093afcf25d28228bcc4be923
Cristian Gutiérrez <instanciageek@gmail.com>
2021-07-05 22:33:29 -0400

    no-history: add interface for create todo

--- app/src/main/java/com/example/todoapp/data/network/TodoApiClient.kt
+++ app/src/main/java/com/example/todoapp/data/network/TodoApiClient.kt
@@ -1,10 +1,16 @@
 package com.example.todoapp.data.network
 
 import com.example.todoapp.data.model.TodoModel
+import okhttp3.RequestBody
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface TodoApiClient {
     @GET("/api/todos")
     suspend fun getAllTodos(): Response<List<TodoModel>>
+
+    @POST("/api/todos")
+    suspend fun saveTodo(@Body requestBody: RequestBody): Response<TodoModel>
 }
