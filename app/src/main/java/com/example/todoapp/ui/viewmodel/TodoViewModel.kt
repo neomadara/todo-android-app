c73f8b53f0b160155f46854a4d65193d69d28d50
Cristian Gutiérrez <instanciageek@gmail.com>
2021-07-05 22:34:18 -0400

    no-history: add save todo in view model

--- app/src/main/java/com/example/todoapp/ui/viewmodel/TodoViewModel.kt
+++ app/src/main/java/com/example/todoapp/ui/viewmodel/TodoViewModel.kt
@@ -1,15 +1,17 @@
 package com.example.todoapp.ui.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.todoapp.data.model.TodoModel
 import com.example.todoapp.domain.GetTodosUseCase
+import com.example.todoapp.domain.SaveTodoUseCase
 import kotlinx.coroutines.launch
 
 class TodoViewModel: ViewModel() {
-    // val todoModel = MutableLiveData<TodoModel>()
-    val todoList = MutableLiveData<List<TodoModel>>()
+    private val TAG = "TodoViewModel"
+    val todoList = MutableLiveData<MutableList<TodoModel>>()
     val isLoading = MutableLiveData<Boolean>()
 
     var getTodosUseCase = GetTodosUseCase()
@@ -20,10 +22,24 @@ class TodoViewModel: ViewModel() {
             val result = getTodosUseCase()
             
             if(!result.isNullOrEmpty()){
-                // todoModel.postValue(result)
-                todoList.postValue(result)
+                todoList.postValue(result as MutableList<TodoModel>?)
                 isLoading.postValue(false)
             }
         }
     }
+
+    fun saveTodo(title: String) {
+        viewModelScope.launch {
+            val saveTodoUseCase = SaveTodoUseCase(title)
+            val result = saveTodoUseCase()
+            Log.d(TAG, "result use case -> $result")
+            if (result is TodoModel) {
+                Log.d(TAG, "todoList -> $todoList")
+                todoList.value?.add(result)
+                todoList.value = todoList.value
+            }
+        }
+    }
+
+    // TODO: create another function for update the list of TODOS called updateListTodos
 }
