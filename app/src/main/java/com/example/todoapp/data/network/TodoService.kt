03cf5e4a592888919f2be8a20972054ddfb79f83
Cristian Gutiérrez <instanciageek@gmail.com>
2021-07-05 22:34:05 -0400

    no-history: add save todo service

--- app/src/main/java/com/example/todoapp/data/network/TodoService.kt
+++ app/src/main/java/com/example/todoapp/data/network/TodoService.kt
@@ -1,17 +1,48 @@
 package com.example.todoapp.data.network
 
+import android.util.Log
 import com.example.todoapp.core.RetrofitHelper
 import com.example.todoapp.data.model.TodoModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.RequestBody.Companion.toRequestBody
+import org.json.JSONObject
 
 class TodoService {
     private val retrofit = RetrofitHelper.getRetrofit()
+    private val TAG = "TodoService"
 
     suspend fun getTodos():List<TodoModel>{
         return withContext(Dispatchers.IO) {
             val response = retrofit.create(TodoApiClient::class.java).getAllTodos()
             response.body() ?: emptyList()
+        }
+    }
+
+    suspend fun saveTodo(todo: TodoModel): TodoModel? {
+        return withContext(Dispatchers.IO) {
+            Log.d(TAG, "todo model -> $todo")
+
+            val jsonObject = JSONObject()
+            jsonObject.put("title", todo.title)
+            val jsonObjectString = jsonObject.toString()
+            val requestBody = jsonObjectString.toRequestBody("application/json".toMediaTypeOrNull())
+            val response = retrofit.create(TodoApiClient::class.java).saveTodo(requestBody)
+
+            Log.d(TAG, "response -> $response")
+            /*if (response.code() == 201) {
+                Log.d(TAG, "response service ok -> ${response.body()}()")
+                response.body()
+                return
+            }*/
+
+            when (response.code()) {
+                201 -> Log.d(TAG, "response service")
+                else -> Log.d(TAG, "response service error")
+            }
+
+            response.body()
         }
     }
 }
