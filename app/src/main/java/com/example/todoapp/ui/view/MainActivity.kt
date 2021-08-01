75221e8997f2e6401d31782186bf1e0a42408805
Cristian Gutiérrez <instanciageek@gmail.com>
2021-07-31 21:23:34 -0400

    no-history: separate ui from observers & add event setOnEditorActionListener to todo name EditText

--- app/src/main/java/com/example/todoapp/ui/view/MainActivity.kt
+++ app/src/main/java/com/example/todoapp/ui/view/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.todoapp.ui.view
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import com.example.todoapp.databinding.ActivityMainBinding
 import com.example.todoapp.ui.adapter.TodoRecyclerAdapter
@@ -24,11 +27,28 @@ class MainActivity : AppCompatActivity() {
         binding.rvTodos.adapter = adapter
 
         setupUI()
+        setupObservers()
     }
 
     private fun setupUI() {
         todoViewModel.getTodos()
 
+        binding.etTodoTitle.setOnEditorActionListener { v, actionId, event ->
+            when (actionId) {
+                EditorInfo.IME_ACTION_DONE -> {
+                    Log.d(TAG, "done")
+                    todoViewModel.saveTodo(binding.etTodoTitle.text.toString())
+                    binding.etTodoTitle.setText("")
+                    val imm: InputMethodManager =
+                        v.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                    imm.hideSoftInputFromWindow(v.windowToken, 0); true
+                }
+                else -> false
+            }
+        }
+    }
+
+    private fun setupObservers() {
         todoViewModel.todoListLiveData.observe(this, {
             adapter.setTodo(it)
         })
@@ -36,22 +56,5 @@ class MainActivity : AppCompatActivity() {
         todoViewModel.isLoadingLiveData.observe(this, {
             binding.progress.isVisible = it
         })
-
-        val dialog = CreateTodoDialogFragment()
-        binding.floatingActionButton.setOnClickListener {
-            dialog.show(supportFragmentManager, "createTodoDialog")
-        }
-
-        todoViewModel.dismissDialog.observe(this, {
-            dialog.dismiss()
-        })
-
-        todoViewModel.dismissDialog.observe(this, {
-            if(it) {
-                Log.d(TAG, "get todos triggerd!")
-                todoViewModel.getTodos()
-            }
-        })
-
     }
 }
