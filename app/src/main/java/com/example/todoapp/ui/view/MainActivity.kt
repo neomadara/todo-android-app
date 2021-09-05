ea49197f35a75c902932db37e5cc5fe0362a6306
Cristian Gutiérrez <instanciageek@gmail.com>
2021-09-05 19:55:14 -0300

    no-history: remove unused code & move service call to TodoActivityScreen

--- app/src/main/java/com/example/todoapp/ui/view/MainActivity.kt
+++ app/src/main/java/com/example/todoapp/ui/view/MainActivity.kt
@@ -12,7 +12,6 @@ import com.example.todoapp.ui.viewmodel.TodoViewModel
 
 class MainActivity : AppCompatActivity() {
     private val todoViewModel: TodoViewModel by viewModels()
-    // private val adapter = TodoRecyclerAdapter()
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,57 +19,17 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // binding.rvTodos.adapter = adapter
-        todoViewModel.getTodos()
-
         binding.composeView.setContent {
             MaterialTheme {
                 TodoActivityScreen(todoViewModel)
             }
         }
-
-        // setupUI()
-    }
-
-    private fun setupUI() {
-        /*todoViewModel.getTodos()
-
-        todoViewModel.todoList.observe(this, {
-            adapter.setTodo(it)
-        })
-
-        todoViewModel.isLoading.observe(this, {
-            binding.progress.isVisible = it
-        })
-
-        todoViewModel.updateTodoList.observe(this, {
-            if (it) todoViewModel.getTodos()
-        })
-
-        val dialog = CreateTodoDialogFragment()
-        binding.floatingActionButton.setOnClickListener {
-            todoViewModel.dismissDialog.postValue(false)
-            dialog.show(supportFragmentManager, "createTodoDialog")
-        }
-
-        todoViewModel.showToastSuccessfully.observe(this, { showToastSuccessfully ->
-            showToastSuccessfully.let {
-                todoViewModel.showToastSuccessfully.value = false
-                Toast.makeText(applicationContext,"this is toast message",Toast.LENGTH_SHORT).show()
-            }
-        })
-
-        todoViewModel.showToastError.observe(this, { showToastError ->
-            showToastError.let {
-                todoViewModel.showToastError.value = false
-                Toast.makeText(applicationContext,"this is toast message",Toast.LENGTH_SHORT).show()
-            }
-        })*/
     }
 }
 
 @Composable
 fun TodoActivityScreen(todoViewModel: TodoViewModel) {
+    todoViewModel.getTodos()
     val todoList by todoViewModel.todoList.observeAsState(initial = emptyList())
 
     TodoScreen(
