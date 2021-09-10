96ae4a68725f67bb45e2e6f5d3468aeaa643a641
Cristian Gutiérrez <instanciageek@gmail.com>
2021-09-10 18:11:41 -0300

    complete a ToDo: add endpoint to complete a todo in retrofit interface

--- app/src/main/java/com/example/todoapp/data/network/TodoApiClient.kt
+++ app/src/main/java/com/example/todoapp/data/network/TodoApiClient.kt
@@ -3,9 +3,7 @@ package com.example.todoapp.data.network
 import com.example.todoapp.data.model.TodoModel
 import okhttp3.RequestBody
 import retrofit2.Response
-import retrofit2.http.Body
-import retrofit2.http.GET
-import retrofit2.http.POST
+import retrofit2.http.*
 
 interface TodoApiClient {
     @GET("/api/todos")
@@ -13,4 +11,7 @@ interface TodoApiClient {
 
     @POST("/api/todos")
     suspend fun saveTodo(@Body requestBody: RequestBody): Response<TodoModel>
+
+    @PUT("/api/todos/{id}/completed")
+    suspend fun completedTodo(@Path("id") todoID: String): Response<Void>
 }
