87098081ea964cd0b5d55ae63ea246ab4dfe6c64
Cristian Gutiérrez <instanciageek@gmail.com>
2021-11-20 19:04:20 -0300

    no-history: implemented inputTodo & Loading composable components

--- app/src/main/java/com/example/todoapp/ui/view/TodoScreen.kt
+++ app/src/main/java/com/example/todoapp/ui/view/TodoScreen.kt
@@ -3,20 +3,11 @@ package com.example.todoapp.ui.view
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.todoapp.data.model.TodoModel
@@ -28,40 +19,12 @@ fun TodoScreen(
     isLoading: Boolean,
     onCompleteTodo: (String) -> Unit
 ) {
-    val todoState = remember { mutableStateOf(TextFieldValue()) }
-
     if (isLoading) {
-        Box(
-            modifier = Modifier.width(100.dp).height(100.dp),
-            contentAlignment = Alignment.Center
-        ) {
-            CircularProgressIndicator(
-                modifier = Modifier.padding(8.dp),
-                strokeWidth = 12.dp,
-            )
-        }
+        LoadingComposable()
     } else {
         Column {
             Text("TODOS", Modifier.align(CenterHorizontally).padding(top = 8.dp))
-            TextField(
-                label = { Text("Enter a TODO") },
-                value = todoState.value,
-                onValueChange = { todoState.value = it },
-                maxLines = 1,
-                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
-                keyboardActions = KeyboardActions(onDone = {
-                    onAddTodo(todoState.value.text)
-                    todoState.value = TextFieldValue()
-                }),
-                modifier = Modifier
-                    .align(CenterHorizontally)
-                    .fillMaxWidth()
-                    .padding(
-                        start = 8.dp,
-                        end = 8.dp,
-                        top = 8.dp
-                    )
-            )
+            TodoInput(onAddTodo)
             TodoList(
                 todos,
                 modifier = Modifier
